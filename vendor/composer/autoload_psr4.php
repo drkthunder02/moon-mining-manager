ad22732175355659b8e4c08f5f4741283e6bbb1d
drkthunder02 <mancuso.christopher@gmail.com>
2020-11-30 13:47:34 -0600

    npm install

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,7 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'phpDocumentor\\Reflection\\' => array($vendorDir . '/phpdocumentor/reflection-common/src', $vendorDir . '/phpdocumentor/type-resolver/src', $vendorDir . '/phpdocumentor/reflection-docblock/src'),
+    'phpDocumentor\\Reflection\\' => array($vendorDir . '/phpdocumentor/reflection-common/src', $vendorDir . '/phpdocumentor/reflection-docblock/src', $vendorDir . '/phpdocumentor/type-resolver/src'),
     'nullx27\\Socialite\\EveOnline\\' => array($vendorDir . '/nullx27/eveonline-socialite/src'),
     'matthewpennell\\Socialite\\EveOnline\\' => array($vendorDir . '/matthewpennell/eveonline-socialite-sisi/src'),
     'jdavidbakr\\ReplaceableModel\\' => array($vendorDir . '/jdavidbakr/replaceable-model/src'),
